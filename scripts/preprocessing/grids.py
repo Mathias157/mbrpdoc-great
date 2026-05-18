7c9d97ddf4ee7fed147c7d1cda4830aee20705ec
mathias157 <mathiasros@proton.me>
2026-05-18 17:08:44 +0200

    Change to Balmorel names

--- scripts/preprocessing/grids.py
+++ scripts/preprocessing/grids.py
@@ -18,12 +18,26 @@ from pathlib import Path
 # Add repo root to path for imports
 sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
+import pandas as pd
 from scripts.utils.plotting import setup_plot
+from scripts.utils.formats import tynd_to_balmorel
 from scripts.utils import load_excel_sheet
+
 
 # ------------------------------- #
 #          1. Functions           #
 # ------------------------------- #
+
+
+def add_new_rows(df: pd.DataFrame, region_list: list, row_number: int, to_or_from: str):
+    for region in region_list:
+        new_row = df.loc[row_number].copy()
+        new_row["Value"] = new_row.Value / len(region_list)
+        new_row[to_or_from] = region
+        df.loc[len(df)] = new_row
+
+    # Set previous row to zero value so the sum will fit later
+    df.loc[row_number, "Value"] = 0
 
 
 # ------------------------------- #
@@ -39,7 +53,40 @@ def main():
         0,
     )
 
-    print(df)
+    sum_before = df.Value.sum()
+
+    # Convert TYNDP region codes to BALMOREL codes
+    # First: Append new rows when Balmorel has more regions
+    length = df.shape[0]
+    for row in range(length):
+        from_region = tynd_to_balmorel[df.From[row]]
+        to_region = tynd_to_balmorel[df.To[row]]
+        if type(from_region) is list:
+            add_new_rows(df, from_region, row, "From")
+        if type(to_region) is list:
+            add_new_rows(df, to_region, row, "To")
+
+    # Second: Convert names
+    length = df.shape[0]
+    for row in range(length):
+        try:
+            from_region = tynd_to_balmorel[df.From[row]]
+            if type(from_region) is str:
+                df.loc[row, "From"] = from_region
+        except KeyError:
+            # print(f"{df.From[row]} already handled")
+            pass
+        try:
+            to_region = tynd_to_balmorel[df.To[row]]
+            if type(to_region) is str:
+                df.loc[row, "To"] = to_region
+        except KeyError:
+            # print(f"{df.To[row]} already handled")
+            pass
+
+    assert sum_before == df.Value.sum(), (
+        "Data cleaning went wrong! Sum of capacities not the same as on input side"
+    )
 
 
 if __name__ == "__main__":
